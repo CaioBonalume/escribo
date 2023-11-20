6282bcd2170db71f06dd6af1456d8366595c5c64
Caio Bonalume <caiobdesigner@gmail.com>
2023-11-20 19:10:48 -0300

    Update escribo_desafio_1

--- escribo_desafio_1.dart
+++ escribo_desafio_1.dart
@@ -1,17 +1,27 @@
 import 'dart:io';
 
 void main() {
-  print('Digite um número inteiro positivo:');
-  int n = int.parse(stdin.readLineSync()!);
-  int soma = 0;
-  if (n > 0) {
-    for (int i = 1; i < n; i++) {
-      if (i % 3 == 0 || i % 5 == 0) {
-        soma += i;
+  bool again = true;
+  while (again) {
+    print('Digite um número inteiro positivo:');
+    String str = stdin.readLineSync()!;
+
+    if (int.tryParse(str) != null) {
+      int n = int.parse(str);
+      int soma = 0;
+      for (int i = 1; i < n; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+          soma += i;
+        }
       }
+      print('Valor digitado: $n \nSoma de divisiveis por 3 e 5: $soma');
+    } else {
+      print('Digite apenas números');
     }
-    print('Valor digitado: $n \nSoma de divisiveis por 3 e 5: $soma');
-  } else if (n != int) {
-    print('Por favor Digite apenas números inteiros');
+    print('Deseja reiniciar? (S/N)');
+    String resp = stdin.readLineSync()!;
+    if(resp.toUpperCase() == 'N') {
+      again = false;
+    }
   }
 }
